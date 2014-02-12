eb49800421e77c5a5835d56e9dd84e9214463718
Raymond Gan <rayning@yahoo.com>
2014-02-12 17:23:39 -0500

    Starting to create Client class and add command line options

--- lib/cbase.rb
+++ lib/cbase.rb
@@ -4,18 +4,24 @@ require "cbase/csv_file"
 require "cbase/company"
 require "cbase/ny_companies"
 
-# require 'net/http'
-# require 'json'
-# require 'csv'
-# require 'open-uri'
-# require 'pry'
+require 'net/http'
+require 'json'
+require 'csv'
+require 'open-uri'
 
-#permalinks = CompanyList.new.get_company_permalinks
-#nycompanies = NYCompanies.new(permalinks).get_ny_companies
+if ARGV.empty?
+  puts "Use like this: 'cbase [arguments]'"
+  exit 1
+end
 
-# PERMALINKS = ['wetpaint', 'kmart', 'viacom', 'mcdonalds', 'facebook', 
-#   'zwinky', 'headstrong-brain-gym', 'uncle-sams-new-york-llc', 'philo-media',
-#   'cargurus', 'emergingcast']
+p ARGV
 
-# nycompanies = NYCompanies.new(PERMALINKS).get_ny_companies
+# permalinks = CompanyList.new.get_company_permalinks
+# nycompanies = NYCompanies.new(permalinks).get_ny_companies
+
+# # PERMALINKS = ['wetpaint', 'kmart', 'viacom', 'mcdonalds', 'facebook', 
+# #   'zwinky', 'headstrong-brain-gym', 'uncle-sams-new-york-llc', 'philo-media',
+# #   'cargurus', 'emergingcast']
+
+# # nycompanies = NYCompanies.new(PERMALINKS).get_ny_companies
 # CsvFile.new(nycompanies).to_csv
---
+++ spec/unit/client_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+# describe Cbase::Client do 
+# end
